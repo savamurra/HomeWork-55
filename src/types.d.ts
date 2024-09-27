0deeccb1f3a8621b3ba2387248a8555161a92f75
Sanzhar Beishekeev <franki-55@mail.ru>
2024-09-27 19:30:37 +0600

    В types.d.ts у меня хранится типы данных для моих обьектов и состояний

---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+export interface IIngredient  {
+    name: string;
+    price: number;
+    image: string;
+}
+
+export interface CountProps {
+    name: string;
+    count: number;
+}
+
+export interface IProps {
+    name: string;
+    count: number;
+}
